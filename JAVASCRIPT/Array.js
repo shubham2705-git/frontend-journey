eab4ed3b4e778732374721f1f1d8079d0cfed7bc
Shubham Ranjan <gkr74226@gmail.com>
2026-09-07 13:54:30 +0530

    Array

--- JAVASCRIPT/Array.js
+++ JAVASCRIPT/Array.js
@@ -40,9 +40,38 @@ Array Methods :\//\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/\/\/\/\///\/\/\/\//\/\\//\/\/\/
 Mutating Methods : \//\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/\/\/\/\///\/\/\/\//\/\\//\/\/\/\/\/\/\/\/\/\\/\/\/\/\/\/\/\/\/\///\/\/\/\//\/\
 let arr = [10,20,30,40,50]
 arr.push(60,70,80)
-Push :::::::::: It is used to add the elements to the end of the array and
+
+push :::::::::: It is used to add the elements to the end of the array and
 it will return the length of the array
 console.log(arr.push(60))
 console.log(arr) // 10,20,30,40,50,60,70,80
 console.log(arr.length) // 8
+
+pop() :::::::::::::::::::::::::::::::::::
+let arr = [10,20,30,40,50]
+arr.pop()
+arr.pop()
+arr.pop(3) // remove only one last element
+console.log(arr)
+
+unshift() :: add the elements at starting ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+let arr = [10,20,30]
+arr.unshift(100,200,300) // add the elements at starting
+console.log(arr)
+
+shift() :: delete first element from starting ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+arr.shift() // delete first single element from starting
+arr.shift()
+arr.shift(4) // value doesnt matter, delete first element from starting
+console.log(arr)
+
+Splice (Starting index, deleteCount, adding elements) ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+let arr1= {10,20,30,40,50}
+console.log(arr1.splice(2)) // 30,40,50 will be deleted
+console.log(arr1.splice(2,2))
+console.log(arr1.splice(3,2,100,200,300)) // 40,50
+console.log(arr1.splice(1,2,90,80,100)) // 10,90,80,100,40,50
+console.log(arr1.splice(2,0,60,70,80,90,100)) // 10,20,60,70,80,90,100,30,40,50
+
+
 */
