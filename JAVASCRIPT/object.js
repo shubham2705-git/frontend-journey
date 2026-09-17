51d6be3044ee3968b963f942fab1b82427630f3c
Shubham Ranjan <gkr74226@gmail.com>
2026-09-17 13:09:42 +0530

    Object

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -1,6 +1,7 @@
 // Object
 // Ways to create object
 // 1. BY USING LITERALS ::::::::::::::::::::::::::::
+/*
 let obj = {
     name : 'Pawan kalyan',
     age:56,
@@ -24,9 +25,34 @@ obj.address.state="TS"
 //Delete
 delete obj.age
 console.log(obj.age)
+*/
 
-
+/*
 // 2. BY USING new KEYWORD :::::::::::::::::::::::::
 let obj2 = new Object()
 obj2.name='jsp'
+obj2.age=43
 console.log(obj2)
+*/    
+
+
+/*
+// 3. BY USING CONSTRUCTOR FUNCTION ::::::::::::::::::
+function person(name,salary,role){
+    this.name = name;
+    this.salary = salary;
+    this.role = role;
+}
+let p1 = new person("rahul",25000,"JSDeveloper");
+let p2 = new person("saruav",15000,"JavaDeveloper");
+let p3 = new person("amit",35000,"SQlDeveloper");
+console.log(p1)
+console.log(p2)
+console.log(p3)
+*/
+
+// 4. USING class KEYWORD :::::::::::::::::::::::::::
+// es6 version
+class Student{
+    
+}
