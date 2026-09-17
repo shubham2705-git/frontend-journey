47413b167e16b113d0fcb903da265e85339b51bb
Shubham Ranjan <gkr74226@gmail.com>
2026-09-17 12:34:23 +0530

    Object

--- JAVASCRIPT/object.js
+++ JAVASCRIPT/object.js
@@ -3,10 +3,20 @@
 let obj = {
     name : 'Pawan kalyan',
     age:56,
-    designation:'DCM'
+    designation:'DCM',
     address:{
         state:'AP',
         city:'bapatla',
     } 
 }
 console.log(obj)
+console.log(obj.name)
+console.log(obj.age)
+console.log(obj.address)
+console.log(obj.address.state)
+console.log(obj.address.city)
+
+// Modify
+obj.name="Akira"
+obj.address.state="TS"
+
