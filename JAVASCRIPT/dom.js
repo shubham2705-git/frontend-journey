52a215fbf102c3920771693bae517e960f42611c
Shubham Ranjan <gkr74226@gmail.com>
2026-09-24 13:21:43 +0530

    dom

--- JAVASCRIPT/dom.js
+++ JAVASCRIPT/dom.js
@@ -58,5 +58,55 @@
 // p.textContent="document object model"
 // document.body.appendChild(p)
 
-// let div=document.createElement("div");
+// let h2=document.createElement("h2");
+// h2.setAttribute("id","span")
+// span.textContent="I am span inside div tag"
+// span.style.display="none"
+// div.append(h2,p1,span)
+
+// // Differences :====================================
+// div.appendChild(p1)
+// div.append(h1,p1,span)
+
+// div.appendChild("this is from appendCHild"); // throws error
+// div.append("this is from append");
+// let div=document.querySelector(".batch.a53")
+// for(let i=1;i<=50;i++){
+//     let button=document.createElement("button")
+//     button.innerText=" This is Button "+i;
+//     div.append(button)
+// }
+// for(let i=1;i<=100;i++){
+//     let h=document.createElement("h1")
+//     h.innerText="Head "+i;
+//     div.append(h)
+// }
+// for(let i=1;i<=200;i++){
+//     let p=document.createElement("p")
+//     p.innerText="para "+i;
+//     div.append(p)
+// }
+
+// let div1=document.createElement('div')
+// div1.textContent="i am div tag from js file"
+// div1.setAttribute('class','batch')
+// div1.setAttribute('class','a53') // it will override previous class
+// div1.removeAttribute('class')
+// console.log(div1)
+
+// document.body.appendChild(div1)
+// div1.remove()
+
+// // classList :=====================================================================================================
+// let div2=document.createElement("div")
+// div2.textContent="i am div tag from js file"
+// div2.classList.add('batch')
+// div2.classList.add('a53')
+// div2.classList.add('js')
+
+// div2.classList.remove('a53')
+// div2.classList.replace('js','a53')
+// console.log(div2)
+// document.body.appendChild(div2)
+
 
